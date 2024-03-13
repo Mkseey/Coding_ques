334475db1ab8762e2f4fb89a2e776abefa567aa6
Mkseey <kartikmahurkar752@gmail.com>
2024-03-13 19:27:17 +0530

    find duplicate num from the array

---
+++ D01/index.js
@@ -0,0 +1,15 @@
+function find_duplicate_Num(){
+    const arr= [ 10,30,40,50,60,50,60,70,70]
+    let duplicateNum = [];
+    for(let num=0; num<arr.length; num++){
+        for(let j=num+1; j<arr.length; j++){
+           if(arr[num]==arr[j]){
+               duplicateNum.push(arr[num]);
+           }
+        }
+    }
+    console.log("duplicate num",duplicateNum);
+
+}
+find_duplicate_Num();
+
