66d4d577e9df6b980a2cf08dba25fcd9d1b31cd3
Mkseey <kartikmahurkar752@gmail.com>
2024-03-23 23:40:06 +0530

    write  the code for the unique num

--- D01/index.js
+++ D01/index.js
@@ -1,15 +1,31 @@
-function find_duplicate_Num(){
+// function find_duplicate_Num(){
+//     const arr= [ 10,30,40,50,60,50,60,70,70]
+//     let duplicateNum = [];
+//     for(let num=0; num<arr.length; num++){
+//         for(let j=num+1; j<arr.length; j++){
+//            if(arr[num]==arr[j]){
+//                duplicateNum.push(arr[num]);
+//            }
+//         }
+//     }
+//     console.log("duplicate num",duplicateNum);
+
+// }
+// find_duplicate_Num();
+
+function find_unique_Num(){
     const arr= [ 10,30,40,50,60,50,60,70,70]
-    let duplicateNum = [];
+    let uniq_num = [];
     for(let num=0; num<arr.length; num++){
         for(let j=num+1; j<arr.length; j++){
-           if(arr[num]==arr[j]){
-               duplicateNum.push(arr[num]);
+           if(arr[num]!=arr[j] && !uniq_num.includes(arr[num]) ){
+            uniq_num.push(arr[num]);
            }
         }
     }
-    console.log("duplicate num",duplicateNum);
+    console.log("unique_num",uniq_num);
 
 }
-find_duplicate_Num();
+find_unique_Num();
+
 
