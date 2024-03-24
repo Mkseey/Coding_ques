3d926122e7f36aed2f8b30c982d94aec37f6ce77
Mkseey <kartikmahurkar752@gmail.com>
2024-03-24 18:02:25 +0530

    created the errors text file and variables notes file and the code snippet for variables

--- D06_variables/index.js
+++ D06_variables/index.js
@@ -1,18 +1,94 @@
-var a = 10;
+/***************************************************************************************/
+/* Developer by:- kartik subhash mahurkar                                          ****/
+/*  */
 
-function func() {
-   var b = 10;
-   // global scope
-   console.log("a: "+a);
+//In the dynamic typed programming or loosly coupled language without using declearing the variable can accesss it.
 
-   // function scope
-   console.log("b: "+b);
-}
 
-func();
 
-// global scope
-console.log("a: "+a);
 
-// function scope
-console.log("b: "+b);
+// a = 10 
+
+// {
+//     console.log("asfsdf",a)
+// }
+// -------------------------------------
+
+// a = 10 
+// console.log("autside",a)
+// {  
+//     a = 20
+//     console.log("asfsdf",a)
+// }
+
+
+// -------------------------------------
+
+
+// a = 10
+// {  
+//     a = 20
+//     console.log("asfsdf",a)
+// }
+
+// -------------------------------------
+
+// for(var i = 0 ; i < 5 ; i++) {
+//     console.log(i);
+//  }
+ 
+//  // i is printed when outside the block
+//  console.log(i); 
+
+// -------------------------------------
+
+
+for(let i = 0 ; i < 5 ; i++) {
+    console.log(i);
+ }
+ 
+ // i is printed when outside the block
+ console.log(i);  // ReferenceError: i is not defined
+// -------------------------------------
+
+// var a = 10;
+
+// console.log("a before re-declaration: "+a)
+
+// function func() {
+//    // re-declared
+//    var a = 20;
+
+//    console.log("a after re-declaration: "+a);
+// }
+
+// func();
+
+// // updated
+// a = 30;
+
+// console.log("a after updation: "+a);
+
+
+
+// -------------------------------------
+
+// var a = 10;
+
+// function func() {
+//    var b = 10;
+//    // global scope
+//    console.log("a: "+a);
+
+//    // function scope
+//    console.log("b: "+b);
+// }
+
+// func();
+
+// // global scope
+// console.log("a: "+a);
+
+// // function scope
+// console.log("b: "+b);
+
