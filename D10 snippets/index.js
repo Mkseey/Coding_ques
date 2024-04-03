e87e674a0c4db300724a2dbbf2dfc3c0087495fa
Mkseey <kartikmahurkar752@gmail.com>
2024-04-03 23:59:24 +0530

    two code snippet added in the snippet folder

---
+++ D10 snippets/index.js
@@ -0,0 +1,142 @@
+
+// code snippet 1
+
+// console.log("start");
+// Promise.resolve().then(()=>{
+//     console.log("promises 1")
+// });
+// setTimeout(()=>console.log("setTimeout 1 "),100);
+// Promise.resolve().then(()=>{
+//     console.log("promises 2")
+// });
+// setTimeout(()=>console.log("setTimeout 2 "),0);
+// console.log("End");
+
+//-----------------------------------------------------------------------------------------
+// code snippet 2
+// console.log("Start");
+// setTimeout(() => console.log("First timeout"), 0);
+// setTimeout(() => console.log("Second timeout"), 100);
+// setTimeout(() => console.log("Third timeout"));
+// console.log("End");
+
+//-----------------------------------------------------------------------------------------
+
+// code snippet 3
+// function outer() {
+//     var count = 0;
+//     function inner() {
+//         count++;
+//         console.log(count);
+//     }
+//     return inner;
+// }
+// const counter = outer();
+// counter(); 
+// counter();
+
+//---------------------------------------------------------------------
+//code snippet 06
+
+// console.log("data",1+ +'2') 
+// console.log("data",1+ +'2'+'2') 
+
+// var a=5;
+// var b= a++;
+// var c =++a;
+// console.log(a,b,c)
+
+//---------------------------------------------------------------------------------
+
+//code snippet 07 
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.getName = function () {
+//     return this.name;
+// };
+
+// var john = new Person('John');
+
+// setTimeout(function () {
+//     console.log(john.getName());
+// }, 1000);
+// console.log('Start');
+
+// setTimeout(function() {
+//     console.log('Timeout');
+// }, 0);
+
+// Promise.resolve().then(function() {
+//     console.log('Promise');
+// });
+
+// console.log('End');
+
+
+
+// function Person(name) {
+//     let names= name 
+//     console.log("asdf",names)
+// }
+
+
+// Person.prototype.getName =  (name) => {
+//     return name;
+// };
+
+// var john = new Person('John');
+
+// setTimeout(function () {
+//     console.log(john.getName());
+// }, 1000);
+// console.log('Start');
+
+// setTimeout(function() {
+//     console.log('Timeout');
+// }, 0);
+
+// Promise.resolve().then(function() {
+//     console.log('Promise');
+// });
+
+// console.log('End');
+
+
+
+
+//--------------------------------------------------
+//code snippet 08
+
+// var myVar = 'outer';
+// function hoistingExample() {
+//     console.log(myVar);
+//     var myVar = 'inner';
+// }
+// hoistingExample();
+
+
+//----------------------------------------------------
+
+//code snippet 09
+
+// function outer() {
+//     var x = 10;
+//     function inner() {
+//         console.log(x);
+//     }
+//     x = 20;
+//     return inner;
+// }
+// var closureFn = outer();
+// closureFn();
+
+
+
+
+
+
+
+
+
