8da44b8796cef11aebe66da9e798faca31c0ff92
Mkseey <kartikmahurkar752@gmail.com>
2024-04-12 13:05:58 +0530

    second longest Word using the sort method implemented

--- D13/index.js
+++ D13/index.js
@@ -8,10 +8,16 @@ function LongestWord(sen) {
         maxLength = arr[i];
       }
     }
-    // code goes here  
     return maxLength; 
-  
-  }
+}
+
+function SecondLongestWord(sen){
+   let arr =  sen.split(" ")
+   arr.sort((a, b) => b.length - a.length);
+   return arr[1]
+}
      
   // keep this function call here 
-  console.log(LongestWord("fun&!! time"));
+  // console.log(LongestWord("fun&!! time"));
+  // console.log(findWordCountLength("mkseey kartik data num"));
+  console.log(SecondLongestWord("mkseey asfsfsfdf data num "));
