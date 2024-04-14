864bd77632f4f4dbdc42c07634706d02ade05a11
Mkseey <kartikmahurkar752@gmail.com>
2024-04-14 18:17:51 +0530

    vowels code the created the code

--- D14 Vowels/index.js
+++ D14 Vowels/index.js
@@ -8,18 +8,45 @@ const vowels = [ 'a', 'e', 'i', 'o', 'u'];
 let count = {};
 
 function countVowels(data) {
-    for(let i= 0 ; i<vowels.length; i++){
-        for(let j= 0;j < data.length; j++){
-            if(vowels[i] === str[j]){
-                console.log("count[vowels[i]]",count[vowels[i]])
-             if (count[vowels[i]]) {
-            count[vowels[i]].push(vowels[i]);
-        } else {
-            count[vowels[i]] = [vowels[i]];
-        }
-            }
+    // for(let i= 0 ; i<vowels.length; i++){
+    //     for(let j= 0;j < data.length; j++){
+    //         if(vowels[i] === str[j]){
+    //             console.log("count[vowels[i]]",count[vowels[i]])
+    //          if (count[vowels[i]]) {
+    //         count[vowels[i]].push(vowels[i]);
+    //     } else {
+    //         count[vowels[i]] = [vowels[i]];
+    //     }
+    //         }
+    //     }
+    // }
+
+
+    // for (let i = 0; i < vowels.length; i++) {
+    //     for (let j = 0; j < data.length; j++) {
+    //         if (vowels[i] === data[j]) {
+    //             if (count[vowels[i]]) {
+    //                 count[vowels[i]]++;
+    //             } else {
+    //                 count[vowels[i]] = 1;
+    //             }
+    //         }
+    //     }
+    // }
+ 
+    for (let i = 0; i < vowels.length; i++) {
+        count[vowels[i]] = {count:0}; // Initialize count for each vowel
+    }
+
+    for (let j = 0; j < data.length; j++) {
+        const char = data[j].toLowerCase(); // Convert to lowercase for case insensitivity
+        if (vowels.includes(char)) {
+            count[char].count++;
         }
     }
+
+
+    
     return count
 }
 const numOfVowels = countVowels(str);
